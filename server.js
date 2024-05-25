2822a96c33dd0b59c47609d3b5f974b89663f0b4
사용자 junyoung011019 <kidjjy5@naver.com>
2024-05-26 01:37:45 +0900

    240526_준영
    방 인원수 확인

--- server.js
+++ server.js
@@ -244,17 +244,41 @@ io.on('connection', (socket) => {
   console.log('A user connected');
   let channel;
   let nickname;
+
+  //최대인원, jwt, id 
+  socket.on('createion',(data)=>{
+    const { MaxCount, channel } = data;
+    channels[channel]={ MaxCount: MaxCount, clients: 1 };
+    currentTime=moment().format('YYYY-MM-DD HH:mm:ss');
+    socket.emit('channelCreated', { message: `Channel ${channel} created : `+ currentTime });
+    console.log("현재 인원 : " +channels[channel].clients);
+  });
+
+  //참석할때 필요한 정보 jwt, 채널 아이디
   socket.on('joinChannel', (data) => {
-      if(channel){
-        socket.leave(channel);
-      }
-      //입력받은 data에서 채널 추출해서 참가 -> 채널번호는 _id로
-      currentTime=moment().format('YYYY-MM-DD HH:mm:ss');
-      channel=data.channel;
-      socket.join(channel);
-      nickname = socket.user.NickName;
-      console.log(`${nickname} joined channel: ${channel}`);
-      io.to(channel).emit('message', { nickname: 'System', message: `${nickname} has joined the channel`,currentTime: `${currentTime}` });
+    //입력받은 data에서 채널 추출해서 참가 -> 채널번호는 _id로
+    currentTime=moment().format('YYYY-MM-DD HH:mm:ss');
+    channel=data.channel;
+    //연결된 채널이 있을 경우
+    if(channel){
+      socket.leave(channel);
+    }
+    //입력한 채널이 없을 경우
+    if (!channel) {
+      socket.emit('error', { message: 'Channel does not exist' });
+      return;
+    }
+    //채널의 최대인원 확인
+    if (channel.clients.length >= channel.maxCount) {
+        socket.emit('error', { message: 'Channel is full' });
+        return;
+    }
+    socket.join(channel);
+    channels[channel].clients+=1;
+    nickname = socket.user.NickName;
+    console.log(`${nickname} joined channel: ${channel}`);
+    console.log("현재 인원 : " +channels[channel].clients);
+    io.to(channel).emit('message', { nickname: 'System', message: `${nickname} has joined the channel`,currentTime: `${currentTime}` });
   });
 
   socket.on('message', (data) => {
@@ -265,9 +289,11 @@ io.on('connection', (socket) => {
   });
 
   socket.on('disconnect', () => {
-      console.log('A user disconnected');
-      io.to(channel).emit('message', { nickname: 'System', message: `${nickname} has lefted the channel`,currentTime: `${currentTime}` });
-      socket.leave(channel);
+    channels[channel].clients-=1;
+    console.log('A user disconnected');
+    io.to(channel).emit('message', { nickname: 'System', message: `${nickname} has lefted the channel`,currentTime: `${currentTime}` });
+    socket.leave(channel);
+    console.log("현재 인원 : " +channels[channel].clients);
   });
 });
 
@@ -288,6 +314,9 @@ app.get('/ShowRecruiting', VerifyJwtAccessToken, async function (req, res) {
       // Recruiting 컬렉션에서 모든 방 정보 조회
       const recruitments = await RecruitingsCollection.find({}).toArray();
       
+      //id랑 현재인원은 소켓에서 
+
+
       // 변환된 모집 정보를 응답으로 전송.
       res.status(200).json(recruitments);
       console.log("리스트 반환 성공");
